e5f70f4375a7d4e9d0d29e3a55f034721234b8c6
Patrick Mireles <patrickm.mireles@gmail.com>
2023-07-10 17:17:46 -0500

    formmodel

--- src/main/java/mireles/codeupspringblog/controllers/PostController.java
+++ src/main/java/mireles/codeupspringblog/controllers/PostController.java
@@ -1,8 +1,8 @@
 package mireles.codeupspringblog.controllers;
-
+import lombok.AllArgsConstructor;
 import mireles.codeupspringblog.Repository.PostRepository;
-
 import mireles.codeupspringblog.Repository.UserRepository;
+import mireles.codeupspringblog.models.EmailService;
 import mireles.codeupspringblog.models.Post;
 import mireles.codeupspringblog.models.User;
 import org.springframework.stereotype.Controller;
@@ -13,72 +13,51 @@ import java.util.List;
 import java.util.Optional;
 
 
+@AllArgsConstructor
 @Controller
-
+@RequestMapping("/posts")
 public class PostController {
-    private PostRepository postRepository;
-    private UserRepository userRepository;
+        private PostRepository postDao;
+        private UserRepository userDao;
+        private EmailService emailService;
 
-    @GetMapping("")
-    public String posts(Model model) {
-        List<Post> posts = postRepository.findAll();
+        @GetMapping("")
+        public String posts(Model model){
+            List<Post> posts = postDao.findAll();
 
-        model.addAttribute("posts",posts);
-        return "/posts/show";
-    }
-
-    @GetMapping("/{id}")
-    public String showSinglePost(@PathVariable Long id, Model model){
-        // find the desired post in the db
-        Optional<Post> optionalPost = postRepository.findById(id);
-        if(optionalPost.isEmpty()) {
-            System.out.printf("Post with id " + id + " not found!");
-            return "home";
+            model.addAttribute("posts",posts);
+            return "/posts/index";
         }
 
-        // if we get here, then we found the post. so just open up the optional
-        model.addAttribute("post", optionalPost.get());
-        return "/posts/index";
+        @GetMapping("/{id}")
+        public String showSinglePost(@PathVariable Long id, Model model){
+            // find the desired post in the db
+            Optional<Post> optionalPost = postDao.findById(id);
+            if(optionalPost.isEmpty()) {
+                System.out.printf("Post with id " + id + " not found!");
+                return "home";
+            }
+
+            // if we get here, then we found the post. so just open up the optional
+            model.addAttribute("post", optionalPost.get());
+            return "/posts/show";
+        }
+
+        @GetMapping("/create")
+        public String showCreate() {
+            return "/posts/create";
+        }
+
+        @PostMapping("/create")
+        public String doCreate(@RequestParam String title, @RequestParam String body) {
+            Post post = new Post();
+            post.setTitle(title);
+            post.setBody(body);
+            User loggedInUser = userDao.findById(1L).get();
+            post.setCreator(loggedInUser);
+            emailService.prepareAndSend(post,title,body);
+            postDao.save(post);
+
+            return "redirect:/posts";
+        }
     }
-
-    @GetMapping("/create")
-    public String showCreate() {
-        return "/posts/create";
-    }
-
-    @PostMapping("/create")
-    public String doCreate(@RequestParam String title, @RequestParam String body) {
-        Post post = new Post();
-        post.setTitle(title);
-        post.setBody(body);
-User loggedInUser = userRepository.findById(1L).get();
-        postRepository.save(post);
-
-        return "redirect:/posts";
-    }
-
-
-
-    @PostMapping("/posts/create")
-    public String createPost(@RequestParam("title") String title,
-                             @RequestParam("body") String body) {
-        // Create a new post
-        Post post = new Post();
-        post.setTitle(title);
-        post.setBody(body);
-
-        // Get a user from the UserRepository (for now, selecting the first user)
-        User user = (User) userRepository.findAll().iterator().next();
-
-        // Assign the user to the post
-
-        // Save the post to the database
-        postRepository.save(post);
-
-        return "redirect:/posts";
-    }
-    public PostController(PostRepository postRepository, UserRepository userRepository) {
-        this.postRepository = postRepository;
-        this.userRepository = userRepository;
-    }
-}
--- src/main/java/mireles/codeupspringblog/models/EmailService.java
+++ src/main/java/mireles/codeupspringblog/models/EmailService.java
@@ -1,6 +1,4 @@
 package mireles.codeupspringblog.models;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
@@ -8,12 +6,12 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-/*
 @Service("mailService")
-*/
+
+
 public class EmailService {
 
-  /*  @Autowired
+  @Autowired
     public JavaMailSender emailSender;
 
     @Value("${spring.mail.from}")
@@ -22,20 +20,16 @@ public class EmailService {
     public void prepareAndSend(Post post, String subject, String body) {
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setFrom(from);
-*//*
         msg.setTo(post.getCreator().getEmail());
-*//**//*
-
-        msg.setTo("patrick@gmail.com");
         msg.setSubject(subject);
         msg.setText(body);
-
         try{
             this.emailSender.send(msg);
         }
         catch (MailException ex) {
             // simply log it and go on...
             System.err.println(ex.getMessage());
-        }*//*
-    }*/
+        }
+    }
+
 }
